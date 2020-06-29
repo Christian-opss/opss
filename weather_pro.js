af25a69e7c3e48a71890e4410c2640a22ca6f233
Christian-opss <62583579+Christian-opss@users.noreply.github.com>
2020-06-29 23:25:28 +0800

    Update weather_pro.js

--- weather_pro.js
+++ weather_pro.js
@@ -6,9 +6,9 @@ let config = {
     darksky_api: "7b6312f5dff188148367c4ba50c89f88", //从https://darksky.net/dev/ 上申请key填入即可
     aqicn_api: "273ef3cefc2a31591f51c0a4333a6073c09a1338", //从http://aqicn.org/data-platform/token/#/ 上申请key填入即可
     huweather_apiKey: "cb689c296b174f2aa4e38770f5c557f0", //和风天气APIkey,可自行前往 https://dev.heweather.com/ 进行获取
-    lat_lon: "23.097158, 113.470545", //请填写经纬度,直接从谷歌地图中获取即可
+    lat_lon: "23.097158_113.470545", //请填写经纬度,直接从谷歌地图中获取即可
     lang: "zh", //语言,随意切换为您想要的语言哦(zh/zh-tw/ja/en/fr/...)
-    log: 1, //调试日志,0为不开启,1为开启,2为开启精简日志
+    log: 0, //调试日志,0为不开启,1为开启,2为开启精简日志
     timeout: 0, //超时时间,单位毫秒(1000毫秒=1秒),一般不推荐修改[为0则不限制超时时间]
     show: {
         //普通每天的
